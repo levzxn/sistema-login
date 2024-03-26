7cf0f53501c66e8b1c98bfd6c71eb0a529828fff
levzxn <levilllzac@gmail.com>
2024-03-26 18:26:06 -0300

    xxdd

--- login-frontend/src/Api/Requests/apiRequests.js
+++ login-frontend/src/Api/Requests/apiRequests.js
@@ -1,32 +1,37 @@
-import { urlCadastro, urlLogin } from "../Config/apiConfig.js";
+import { urlCadastro, urlLogin, urlTeste } from "../Config/apiConfig.js";
 
-const loginUsuario = async (dados) =>{
-    const conexao = await fetch(urlLogin,{
-        method:'POST',
-        headers:{
-            'Content-type':'application/json'
+const loginUsuario = async (dados) => {
+    const conexao = await fetch(urlLogin, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
         },
-        body:JSON.stringify({
+        body: JSON.stringify({
             username: dados.nomeUsuario,
             password: dados.senha
         })
     })
-    if(!conexao.status === 200){
-        return(
+    if (!conexao.status === 200) {
+        return (
             <div>
                 <h1>Usuário/Senha não encontrados</h1>
             </div>
         )
     }
+    else {
+        const teste = await fetch(urlTeste)
+        const json = teste.json()
+        console.log(json)
+    }
 }
 
-const cadastroUsuario = async (dados) =>{
-    const conexao = await fetch(urlCadastro,{
-        method:'POST',
-        headers:{
-            'Content-type':'application/json'
+const cadastroUsuario = async (dados) => {
+    const conexao = await fetch(urlCadastro, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
         },
-        body:JSON.stringify({
+        body: JSON.stringify({
             username: dados.nomeUsuario,
             password: dados.senha
         })
